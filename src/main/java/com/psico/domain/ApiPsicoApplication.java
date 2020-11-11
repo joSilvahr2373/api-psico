4eeb7ce7c84ffa98296720da07fb3fb8e4865c79
Josué Mendes <johr2373@gmail.com>
2020-11-11 19:34:43 -0300

    initial commit

---
+++ src/main/java/com/psico/domain/ApiPsicoApplication.java
@@ -0,0 +1,13 @@
+package com.psico.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPsicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPsicoApplication.class, args);
+	}
+
+}
